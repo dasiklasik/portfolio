8935b9e4495273cf42b27e23cf85e7bc1a7c9ac2
dasiklasik <daryasamsonowicz@gmail.com>
2022-08-15 12:13:25 +0300

    style contacts

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -6,12 +6,13 @@ export const Contacts = () => {
     return (
         <div id={'contacts'} className={styles.block}>
             <div className={'wrapper'}>
-                <h2>Самсонович Дарья</h2>
+                <h2>Darya Samsanovich</h2>
                 <div className={styles.flexWrapper}>
-                    <div className={styles.contactItem}></div>
-                    <div className={styles.contactItem}></div>
-                    <div className={styles.contactItem}></div>
-                    <div className={styles.contactItem}></div>
+                    <ul>
+                        <li><a href='https://t.me/DaryaSams' target='_blank'>Telegram</a></li>
+                        <li><a href='https://www.linkedin.com/in/%D0%B4%D0%B0%D1%80%D1%8C%D1%8F-%D1%81%D0%B0%D0%BC%D1%81%D0%BE%D0%BD%D0%BE%D0%B2%D0%B8%D1%87-7541a5235/' target='_blank'>Linkedin</a></li>
+                        <li><a href='https://github.com/dasiklasik' target='_blank'>GitHub</a></li>
+                    </ul>
                 </div>
             </div>
         </div>
--- src/components/HireBlock/HireBlock.tsx
+++ src/components/HireBlock/HireBlock.tsx
@@ -7,7 +7,10 @@ export const HireBlock = () => {
         <div className={styles.block}>
             <div className={'wrapper'}>
                 <h2>I'm considering remote work options</h2>
-                <a href={''} className={styles.link}>Hire me</a>
+                <a href='https://www.linkedin.com/in/%D0%B4%D0%B0%D1%80%D1%8C%D1%8F-%D1%81%D0%B0%D0%BC%D1%81%D0%BE%D0%BD%D0%BE%D0%B2%D0%B8%D1%87-7541a5235/'
+                   className={styles.link}
+                   target={'_blank'}
+                >Hire me</a>
             </div>
         </div>
     )
